6c1fc6019f75595f751de2e75806b4b6a1a57b49
luoning <243539594@qq.com>
2017-05-28 15:40:20 +0800

    111

--- src/main/java/com/hz/hzdjr/example/service/DemoService.java
+++ src/main/java/com/hz/hzdjr/example/service/DemoService.java
@@ -1,6 +1,11 @@
 package com.hz.hzdjr.example.service;
 
 public interface DemoService {
+	/**
+	 * @param 1
+	 *
+	 *
+	 * */
 	public String showMessage();
 	public String showParamsMessage(String param);
 }
